96ef88d188754b2be0fd99b02db7f2ae5dbcf0d7
Giap Hoang Van <giap31032004@ap31032004gmail.com>
2025-11-25 14:19:35 +0700

    ipc

--- main.c
+++ main.c
@@ -3,10 +3,15 @@
 #include "process.h"
 #include "task.h"
 #include "sync.h" 
+#include "ipc.h"
+#include <stdint.h>
+
+
 /* --- CẤU HÌNH HỆ THỐNG --- */
 #define SYSTEM_CLOCK      80000000
 #define SYSTICK_RATE      2000000   // 10 tick/giây (đảo task nhanh hơn để thấy phản ứng)
 
+os_msg_queue_t temp_queue; // Hàng đợi tin nhắn cho nhiệt độ
 os_mutex_t app_mutex; // chiếc khóa chung cho cả hệ thống
 
 void delay(volatile unsigned int count) {
@@ -18,6 +23,10 @@ void delay(volatile unsigned int count) {
 /* --- MAIN --- */
 void main(void) {
     uart_init();
+    process_init();
+
+    msg_queue_init(&temp_queue);
+    mutex_init(&app_mutex);
     
     uart_print("\033[2J"); // Lệnh xóa màn hình terminal (nếu hỗ trợ)
     uart_print("MyOS IoT System Booting...\r\n");
--- task.c
+++ task.c
@@ -1,53 +1,57 @@
 #include "task.h"
 #include "uart.h"
-#include "process.h" // Thêm cái này để dùng os_delay
-#include "sync.h"    // Thêm cái này để dùng mutex
+#include "process.h" 
+#include "sync.h"    
+#include "ipc.h"
 #include <stdint.h>
 
 /* Biến toàn cục */
 volatile int current_temperature = 25; 
 volatile int system_uptime = 0;
 extern os_mutex_t app_mutex;
+extern os_msg_queue_t temp_queue;
 
 /* TASK 1: SENSOR */
 void task_sensor_update(void) {
+    int local_temp = 25; 
     int direction = 1; 
     while (1) {
         os_delay(10); 
-
-        mutex_lock(&app_mutex);
-        if (direction == 1) {
-            current_temperature += 5;
-            if (current_temperature >= 55) direction = -1; 
+        
+        if(direction == 1){
+            local_temp += 5;
+            if(local_temp >= 55) direction = -1;
         } else {
-            current_temperature -= 5;
-            if (current_temperature <= 20) direction = 1;
+            local_temp -= 5;
+            if(local_temp <= 20) direction = 1;
         }
-        mutex_unlock(&app_mutex);
+
+        msg_queue_send(&temp_queue, local_temp);
     }
 }
 
 /* TASK 2: DISPLAY */
 void task_display(void) {
-    int last_temp = -999; 
+    int received_temp; 
 
     while (1) {
-        // SỬA: Đưa lock vào trong vòng lặp
-        mutex_lock(&app_mutex);
+        // 1. Nhận dữ liệu từ hàng đợi IPC
+        received_temp = msg_queue_receive(&temp_queue);
         
-        if (current_temperature != last_temp) {
+        // 2. Cập nhật biến toàn cục (để Task Alarm dùng nếu cần)
+        current_temperature = received_temp;
+
+        // 3. In ra màn hình giá trị VỪA NHẬN ĐƯỢC
+        mutex_lock(&app_mutex);
             uart_print("----------------------\r\n");
             uart_print("| Temp: "); 
-            uart_print_dec(current_temperature); 
+            
+            // SỬA QUAN TRỌNG: In received_temp (số mới) thay vì current_temperature (số cũ)
+            uart_print_dec(received_temp); 
+            
             uart_print(" C         |\r\n");
             uart_print("----------------------\r\n");
-            last_temp = current_temperature; 
-        }
-        
-        // SỬA: Unlock ngay sau khi in xong
         mutex_unlock(&app_mutex);
-        
-        os_delay(20); // Tăng delay lên chút cho dễ nhìn
     }
 }
 
@@ -56,18 +60,22 @@ void task_alarm(void) {
     int alarm_active = 0; 
 
     while (1) {
-        os_delay(10);
+        os_delay(5);
         
         mutex_lock(&app_mutex);
         if (current_temperature > 40) {
             if (alarm_active == 0) {
+                uart_print("\r\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\r\n");
                 uart_print("\r\n!!! [ALARM] WARNING: OVERHEAT !!!\r\n");
+                uart_print("\r\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\r\n");
                 alarm_active = 1; 
             }
         } 
         else {
             if (alarm_active == 1) {
-                uart_print("\r\n[ALARM] Temperature Normal.\r\n");
+                uart_print("\r\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\r\n");
+                uart_print("\r\n[ALARM] Temperature Normal.\r\n");                
+                uart_print("\r\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\r\n");
                 alarm_active = 0; 
             }
         }
